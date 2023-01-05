a50161338273ad9989accb79694be037cff2f14c
kana03 <camille.naulet@hotmail.fr>
2023-01-05 16:54:25 +0100

    :fire: deploy

--- app.js
+++ app.js
@@ -9,23 +9,25 @@ const userRoutes = require('./routes/user');
 const wasteRoutes = require('./routes/waste');
 
 const app = express();
-app.use(helmet())
-  .use(compression())
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, UserID, Email');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+// app.use(helmet())
+//   .use(compression())
+//   .use(bodyParser.json())
+//   .use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, UserID, Email');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//     next();
+//   });
 
 
 mongoose.connect("mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASSWORD+"@cluster0.n6g726d.mongodb.net/?retryWrites=true&w=majority")
   .then(() => logger.info('✅ Successfully connected to the database'))
   .catch((e) => logger.error(`⛔️ Error during database connection - ${e}`))
 
+app.use(express.json({extended: false}));
 
-app.use('/api/auth', userRoutes)
+app
+    // .use('/api/auth', userRoutes)
   .use('/api/wastes', wasteRoutes);
 
 module.exports = app;
