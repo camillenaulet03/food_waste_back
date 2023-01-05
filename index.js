9dc0174a1e2ec02dbd9ebbfad6c2ae69072c9fa1
kana03 <camille.naulet@hotmail.fr>
2023-01-05 17:18:33 +0100

    :fire: deploy

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8086;
 const app = require('./app');
 
 const server = http.createServer(app);
