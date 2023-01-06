f4b6792f6192433bdb34db0c2d9459c8f3b321da
kana03 <camille.naulet@hotmail.fr>
2023-01-06 12:06:26 +0100

    deploy

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const http = require('http');
 const port = process.env.PORT || 8080;
 const app = require('./app');
 
-const server = http.createServer(app);
+const index = http.createServer(app);
 
-server.listen(port);
+index.listen(port);
 
 console.log('Server created');
 console.log('Listen on port ', port);
